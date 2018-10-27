f38bf30289f8b9bfaa02d7f202a0f46f755b57ba
c-bata <contact@c-bata.link>
2018-10-27 20:38:48 +0900

    Use netinet/x.h

--- main.c
+++ main.c
@@ -8,12 +8,13 @@
 #include <sys/types.h>
 #include <sys/ioctl.h>
 #include <net/bpf.h>
+#include <netinet/tcp.h>
+#include <netinet/ip.h>
+#include <netinet/if_ether.h>
 #include <netinet/in.h>
 #include <net/if.h>
+#include <arpa/inet.h>
 
-#include "ethernet.h"
-#include "ip.h"
-#include "tcp.h"
 #include "bpf.h"
 
 int main()
@@ -34,54 +35,44 @@ int main()
     while((dataLength = readBpfPacketData(&sniffer, &info)) != -1) {
         printf("--------------------------------------------------\n");
         printf("Payload length: %d\n", dataLength);
-        EthernetHeader* ethHeader = (EthernetHeader*)info.data;
+        struct ether_header* eh = (struct ether_header*)info.data;
         printf(" Ethernet Frame\n");
         printf("  src mac address: %x:%x:%x:%x:%x:%x\n",
-               ethHeader->srcAddress[0],
-               ethHeader->srcAddress[1],
-               ethHeader->srcAddress[2],
-               ethHeader->srcAddress[3],
-               ethHeader->srcAddress[4],
-               ethHeader->srcAddress[5]);
+               eh->ether_shost[0],
+               eh->ether_shost[1],
+               eh->ether_shost[2],
+               eh->ether_shost[3],
+               eh->ether_shost[4],
+               eh->ether_shost[5]);
 
         printf("  dst mac address: %x:%x:%x:%x:%x:%x\n",
-               ethHeader->dstAddress[0],
-               ethHeader->dstAddress[1],
-               ethHeader->dstAddress[2],
-               ethHeader->dstAddress[3],
-               ethHeader->dstAddress[4],
-               ethHeader->dstAddress[5]);
+               eh->ether_dhost[0],
+               eh->ether_dhost[1],
+               eh->ether_dhost[2],
+               eh->ether_dhost[3],
+               eh->ether_dhost[4],
+               eh->ether_dhost[5]);
 
+        if (ntohs(eh->ether_type) == ETHERTYPE_IP) {
+            printf("  type: IPv4, %x\n", eh->ether_type);
 
-        if (ethHeader->type == TYPE_IPV4) {
-            printf("  type: IPv4, %x\n", ethHeader->type);
-
-            IpHeader* ip = (IpHeader*)((long)ethHeader + sizeof(EthernetHeader));
-
+            struct ip* ip = (struct ip*)((long)eh + sizeof(struct ether_header));
             printf(" IP Frame\n");
-            printf("  headerLength: %d\n", ip->headerLength * 4);
-            printf("  version: %d\n", ip->version);
-            printf("  protocol: %d\n", ip->protocol);
-            printf("  ttl: %d\n", ip->ttl);
-            printf("  dst ip: %d.%d.%d.%d\n",
-                   ip->dstAddress[0],
-                   ip->dstAddress[1],
-                   ip->dstAddress[2],
-                   ip->dstAddress[3]);
-            printf("  src ip: %d.%d.%d.%d\n",
-                   ip->srcAddress[0],
-                   ip->srcAddress[1],
-                   ip->srcAddress[2],
-                   ip->srcAddress[3]);
+            printf("  headerLength: %d\n", ip->ip_hl * 4);
+            printf("  version: %d\n", ip->ip_v);
+            printf("  protocol: %d\n", ip->ip_p);
+            printf("  ttl: %d\n", ip->ip_ttl);
+            printf("  dst ip: %s\n", inet_ntoa(ip->ip_dst));
+            printf("  src ip: %s\n", inet_ntoa(ip->ip_src));
 
-            if (ip->protocol == IP_PROTOCOL_TCP) {
-                TCPHeader* tcp = (TCPHeader*)((long)ip + (ip->headerLength * 4));
-                printf(" TCP Frame\n");
-                printf("  dst port: %d\n", tcp->dstPort);
-                printf("  src port: %d\n", tcp->srcPort);
+            if (ip->ip_p == IPPROTO_TCP) {
+                struct tcphdr* tcp = (struct tcphdr*)((long)ip + (ip->ip_hl * 4));
+                printf(" TCP Packet\n");
+                printf("  dst port: %d\n", tcp->th_dport);
+                printf("  src port: %d\n", tcp->th_sport);
             }
         } else {
-            printf("  type: Other, %x\n", ethHeader->type);
+            printf("  type: Other, %x\n", eh->ether_type);
         }
     }
     closeBpfSniffer(&sniffer);
